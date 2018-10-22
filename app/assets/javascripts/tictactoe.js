b75161192b9bb63039cea5838c84ac754ec95389
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-22 20:26:33 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,2 +1,11 @@
 const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], 
                         [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+var turn = 0;
+var currentGame = 0;
+
+$(document).ready(function() {
+  attachListeners();
+});
+
+var player = () => turn % 2 ? 'O' : 'X'; 
